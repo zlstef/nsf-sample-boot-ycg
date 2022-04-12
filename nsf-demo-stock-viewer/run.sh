d2c3f8613bb15982fc9070fb2ee268abc489dcc3
chenggangyang <1451571361@qq.com>
2022-04-12 17:11:44 +0800

    Create run.sh

---
+++ nsf-demo-stock-viewer/run.sh
@@ -0,0 +1,6 @@
+
+if [ "in-nsf" = "$1" ]; then
+    java -javaagent:./nsf-javaagent-home/nsf-agent-premain-1.0.jar=debug -jar target/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+else
+    java -jar target/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+fi;
