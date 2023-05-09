0cf0682aab5b896dc52b53dec155ef147a802494
wanchu <1451571361@qq.com>
2023-05-09 10:41:11 +0800

    Update run.sh

--- nsf-demo-stock-viewer/run.sh
+++ nsf-demo-stock-viewer/run.sh
@@ -1,6 +1,2 @@
-
-if [ "in-nsf" = "$1" ]; then
-    java -javaagent:./nsf-javaagent-home/nsf-agent-premain-1.0.jar=debug -jar target/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
-else
-    java -jar target/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
-fi;
+#!/usr/bin/env bash
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/skywalking-napm-bin-8.10.0-latest/agent/skywalking-agent.jar -Dskywalking_config=/opt/apm/skywalking-napm-bin-8.10.0-latest/agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
