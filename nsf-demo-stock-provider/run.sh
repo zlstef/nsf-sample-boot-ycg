2e6de8cf92a58bcf550f156d01c8d014f411fba7
zlstef <z.liang@msn.com>
2023-06-05 17:45:55 +0800

    Update run.sh

--- nsf-demo-stock-provider/run.sh
+++ nsf-demo-stock-provider/run.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-echo ${JAVA_OPTS}
-java ${JAVA_OPTS} -jar /opt/nsf/nsf-demo-stock-provider-1.0-SNAPSHOT.jar
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/skywalking-napm-bin-8.10.0-latest/agent/skywalking-agent.jar -Dskywalking_config=/opt/apm/skywalking-napm-bin-8.10.0-latest/agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-provider-1.0-SNAPSHOT.jar
